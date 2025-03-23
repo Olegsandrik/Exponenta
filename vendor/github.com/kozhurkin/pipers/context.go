89d2a3c0238bc0e3a05d2f6b7805760486e31599
Olegsandrik <nikitinav2@student.bmstu.ru>
2025-03-23 13:03:04 +0300

    delete vendor

--- vendor/github.com/kozhurkin/pipers/context.go
+++
@@ -1,9 +0,0 @@
-package pipers
-
-import "context"
-
-type PipersContext struct {
-	context.Context
-	Limit    int
-	TailDone chan struct{}
-}
--- vendor/github.com/kozhurkin/pipers/errors.go
+++
@@ -1,7 +0,0 @@
-package pipers
-
-type Errors []error
-
-//func (errs Errors) Join() error {
-//	return errors.Join(errs...)
-//}
