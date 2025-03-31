0d8f173036dec78b80b046cf17dc364a3a7b328a
Olegsandrik <nikitinav2@student.bmstu.ru>
2025-03-31 18:04:13 +0300

    fix migrations

--- db/migrations/20250331093319_null_images.sql
+++ db/migrations/20250331093319_null_images.sql
@@ -5,4 +5,5 @@ UPDATE ingredients SET image='null.jpg'  WHERE image IS NULL;
 
 -- +goose Down
 -- +goose StatementBegin
+UPDATE ingredients SET image=null WHERE image='null.jpg';
 -- +goose StatementEnd
